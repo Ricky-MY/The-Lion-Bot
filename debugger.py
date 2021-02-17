7cd4de8b20296f2d1ed35c2d7281036a706c604a
Ricky <66440980+Ricky-MY@users.noreply.github.com>
2021-02-17 23:58:35 +0630

    Mass deletion

--- debugger.py
+++ debugger.py
@@ -15,3 +15,16 @@ for word in f:
 	pyautogui.press("enter")
 	time.sleep(2)
   
+  
+def get_prefix(client, message):
+	with open('prefixes.json', 'r') as f:
+		prefixes = load(f)
+	try:
+		return commands.when_mentioned_or(prefixes[str(message.guild.id)])(client, message)
+	except KeyError:
+		with open('prefixes.json', 'r') as f:
+			prefixes = load(f)
+		prefixes[str(message.guild.id)] = "/"
+		with open('prefixes.json', 'w') as f:
+			dump(prefixes, f, indent=4)
+		return commands.when_mentioned_or(prefixes[str(message.guild.id)])(client, message)
