61bb3be5ff3c6834f55b1aaffd6a20bccff09c0b
Ricky <66440980+Ricky-MY@users.noreply.github.com>
2021-03-09 22:08:58 +0630

    documentation

--- bot/moderation/moderation.py
+++ bot/moderation/moderation.py
@@ -11,21 +11,24 @@ class Moderation(commands.Cog):
 		self.client = client
 		self.color = 0xC0C0C0
 
-	@commands.command()
+	@commands.command(name="kick")
 	@commands.has_guild_permissions(kick_members=True)
 	async def kick(self, ctx, member: discord.Member, *, reason):
 		await member.kick(reason=reason)
 		await ctx.send(f"🚴🦶 {member.mention} has been kicked for {reason} by {ctx.author.mention}")	
 
-	@commands.command()
+	@commands.command(name="ban")
 	@commands.has_guild_permissions(ban_members=True)
 	async def ban(self, ctx, member: discord.Member, *, reason):
+		"""Places a permanent ban on a user."""
 		await member.ban(reason=reason)
 		await ctx.send(f"🚴🔨 {member.mention} has been banned for {reason} by {ctx.author.mention}.")
 
-	@commands.command()
+	@commands.command(name="unban")
 	@commands.has_guild_permissions(ban_members=True)
 	async def unban(self, ctx, id_ , *, reason = "Unspecified"):
+		"""Lifts an active ban on a user."""
+
 		bans = await ctx.guild.bans()
 		for ban_entry in bans:
 			if ban_entry.user.id == int(id_):
@@ -33,9 +36,11 @@ class Moderation(commands.Cog):
 				await ctx.send(f"🛂 {ban_entry.user.mention} has been unbanned by {ctx.author.mention}.")
 				return
 
-	@commands.command()
+	@commands.command(name="clear", aliases=["purge"])
 	@commands.has_guild_permissions(manage_messages=True)
 	async def clear(self, ctx, amount=5, user : discord.Member = None):
+		"""Removes a certain amount of messages from the channel of which the command is used in. If the amount isn't specified, it will default to 5."""
+
 		channel = ctx.channel
 		def is_user(message):
 			if user is not None:
