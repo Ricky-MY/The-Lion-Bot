1b2b4e084ec7e53ef434232eb47340dfe3f88339
Ricky <66440980+Ricky-MY@users.noreply.github.com>
2021-03-09 22:08:43 +0630

    Help rewrite
    1. Uses yaml prompts
    2. Better asthetics

--- bot/utilities/help.py
+++ bot/utilities/help.py
@@ -1,180 +1,134 @@
 import discord
+import yaml
 
 from discord.ext import commands
 
 from bot.utilities.prefixes import Prefixes
 
+
 class Help(commands.Cog):
 
-	def __init__(self, bot):
-		self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
+        with open("config.yaml", 'r') as file:
+            config = yaml.load(file, Loader=yaml.SafeLoader)
+        self.color = config["asthetics"]["mainColor"]
 
-	@commands.group(name=str("help"), invoke_without_command=True)
-	@commands.guild_only()
-	async def help(self, ctx):
-		prefix = Prefixes.fetch_prefix(ctx.guild.id)
-		embed = discord.Embed(color=discord.Colour.dark_gold())
-		fields = [('[Moderation]',
-				   f"`{prefix}help moderation`\nThe moderation module includes all basic moderation commands, for each command you need the corresponding server permission, for isntance, a person will be able to ban people if they themselves have the ban_members permission for the guild.",
-				   False),
-				  ('[Miscellaneous]',
-				   f"`{prefix}help misc`\nA guide to all miscellaneous commands that exists, this includes a wide variety of utilities that can furthermore increase your capability as a server owner.",
-				   True),
-				  ('[Greeting]',
-				   f"`{prefix}help greet`\nA customizable welcoming system that you could use to welcome people",
-				   True),
-				  ('[MemberCounter]',
-				   f"`{prefix}help mbc`\nA module that allows you to setup a voice channel as a member counter that changes its name to the amount of members in ur server",
-				   True),
-				  ('[Tag]',
-				   f"`{prefix}help tag`\nA module that allows you store text that can be recalled with a name",
-				   True),
-				  ('[TypeWriter]',
-				   f"`{prefix}help typewriter`\nA module that allows you create, send, edit and fully customize an embed.",
-				   True),
-				  ('[Magic]', f"`{prefix}help magic`\nMystic arts from harry potter", True)
-				  ]
-		for name, value, inline in fields:
-			embed.add_field(name=name, value=value, inline=inline)
-		embed.set_author(name="-=The Lion's Help Menu=-")
-		embed.add_field(name='[Levelling]',
-						value=f"`{ctx.prefix}help level`\nA chat levelling system that can be used to allow people to level up corresponding to the messages they send")
-		#	embed.add_field(name='[ReactionRole]', value=f"`{ctx.prefix}help rrole`\nA reaction role function that allows you to setup reactionroles for messages")
-		await ctx.send(embed=embed)
+        with open(config["directories"]["prompts"], encoding="utf-8") as file:
+            self.prompts = yaml.load(file, Loader=yaml.SafeLoader)["help"]
+        self.embed = discord.Embed(color=self.color)
+        self.embed.set_author(
+            name="Pill's help menu", icon_url=self.prompts["main"]["author_url"])
+        self.embed.set_footer(
+            text="Type p!help cmd <command> for an in-depth command help")
 
-	@help.command()
-	@commands.guild_only()
-	async def moderation(self, ctx):
-		prefix = Prefixes.fetch_prefix(ctx.guild.id)
-		embed = discord.Embed(title='=Moderation Commands=', color=discord.Color.dark_gold())
-		fields = [('Kick', f'`{prefix}kick (@user) [reason]`'),
-				  ('Ban', f'`{prefix}ban (@user) [reason]`'),
-				  ('UnBan', f'`{prefix}unban (@user) [reason]`'),
-				  ('Mute', f'`{prefix}mute (@user)`'),
-				  ('UnMute', f'`{prefix}unmute (@user)`')
-				  ]
-		for name, value in fields:
-			embed.add_field(name=name, value=value)
-		await ctx.send(embed=embed)
+    @commands.group(name=str("help"), invoke_without_command=True)
+    @commands.guild_only()
+    async def help(self, ctx):
+        prefix = Prefixes.fetch_prefix(ctx.guild.id)
+        embed = self.embed.copy()
+        embed.description = self.prompts["main"]["desc"]
+        fields = [('⚒️ Moderation', f"**[{prefix}help moderation](https://google.com \"Moderation related help\")**"),
+                  ('🧰 Miscellaneous',
+                   f"**[{prefix}help misc](https://google.com \"Miscellaneous commands\")**"),
+                  ('🙏 Greeting',
+                   f"**[{prefix}help greet](https://google.com \"Welcome and goodbye messages\")**"),
+                  ('🛡️ Aegis',
+                   f"**[{prefix}help aegis](https://google.com \"Anti-nuke and security system\")**"),
+                  ('🛑 Tag',
+                   f"**[{prefix}help tag](https://google.com \"Global and guild tags\")**"),
+                  ('✍️ TypeWriter',
+                   f"**[{prefix}help typewriter](https://google.com \"Embed creation command\")**"),
+                  ('🪄 Magic', f"**[{prefix}help magic](https://google.com)**")]
 
-	@help.command()
-	@commands.guild_only()
-	async def magic(self, ctx):
-		fembed = discord.Embed(title="=Grand Warden's Handbook=",
-							   description='poop, slap, hug, diceroll, 8ball, ping, gift, rickroll, joke',
-							   color=discord.Color.dark_gold())
-		await ctx.send(embed=fembed)
+        for name, value in fields:
+            embed.add_field(name=name, value=value, inline=True)
+        embed.set_thumbnail(
+            url=self.prompts["main"]["thumbnail"])
+        await ctx.send(embed=embed)
 
-	@help.command()
-	@commands.guild_only()
-	async def misc(self, ctx):
-		prefix = Prefixes.fetch_prefix(ctx.guild.id)
-		embed = discord.Embed(title='=Miscellaneous Commands=', color=discord.Color.dark_gold())
-		fields = [('Users', f'`{prefix}users`\nReturns the amount of members in a server'),
-				  ('Addrole',
-				   f'`{prefix}addrole <name> (hex_color_code) (displays_seperately True/False)`\nAdds a role'),
-				  ('Giverole', f'`{prefix}giverole <member> <role>`\nAssigns a role to the member tagged'),
-				  ('Delrole', f'`{prefix}delrole (@role)`\nDeletes a role'),
-				  ('Avatar', f'{prefix}avatar (@member)`\nReturns the avatar of the member tagged'),
-				  ('Prefix', f'`{prefix}prefix`\nChecks the current server prefix'),
-				  ('Announce', f'`{prefix}announce <#channel> <message>`\nAnnounces a text message with a pre-built template.'),
-				  ('Announce here', f'`{prefix}announce here <#channel> <message>`\nAnnounces a text message with a pre-built template and mentions everyone online.'),
-				  ('Announce Everyone', f'`{prefix}announce everyone <#channel> <message>`\nAnnounces a text message with a pre-built template and mentions everyone.'),
-				  ('Userinformation',
-				   f'`{prefix}userinfo <member>`\nDisplays the tagged user info, will disaply your info if none is mentioned')
-				  ]
-		for name, value in fields:
-			embed.add_field(name=name, value=value)
-		await ctx.send(embed=embed)
+    @help.command(name="moderation")
+    @commands.guild_only()
+    async def moderation(self, ctx):
+        embed = self.embed.copy()
+        embed.title = "⚒️ Moderation"
+        embed.description = self.prompts["moderation"]["desc"]
+        await ctx.reply(embed=embed, mention_author=False)
 
-	@help.command()
-	@commands.guild_only()
-	async def aegis(self, ctx):
-		prefix = Prefixes.fetch_prefix(ctx.guild.id)
-		embed = discord.Embed(title='=Acessing Aegis=', color=discord.Color.dark_gold())
-		embed.add_field(name='To access:', value=f'`Execute the "{prefix} aegis" command to acess the module`')
-		await ctx.send(embed=embed)
+    @help.command(name="magic")
+    @commands.guild_only()
+    async def magic(self, ctx):
+        embed = self.embed.copy()
+        embed.title = "🪄 Magic"
+        embed.description = self.prompts["magic"]["desc"]
+        await ctx.reply(embed=embed, mention_author=False)
 
-	@help.command()
-	@commands.guild_only()
-	async def greet(self, ctx):
-		prefix = Prefixes.fetch_prefix(ctx.guild.id)
-		embed = discord.Embed(title="=Greeting System Guide=",
-							  description='This system allows you to send customizable welcome messages when a member joins your server!',
-							  color=discord.Color.dark_gold())
-		fields = [('Setup',
-				   f'`{prefix}welcome <channel> <hex_color_code> <msg>`\n<channel> : Mention the channel you would like to use for the greeting message\n<msg> : _Type your welcome message here(you can use the special codes from below)_\n<hex-color-code> : _Hex color code for your welcome embed message, use 000000 for default color_'),
-				  ('Check', f'`{prefix}welcome check`\n Checks what the current preferences for welcome are!'),
-				  ('Clear', f'`{prefix}welcome clear`\n Clears and disables the greeting system!'),
-				  ('Message Limitations',
-				   f'_Adding the keywords down below would perform a unique action that allows you to better customize your message!_\n`{{mention}}`: Mentions the new member\n`{{guild}}`: Says the guild name\n`{{user}}`: Says the name of the new member')
-				  ]
-		for name, value in fields:
-			embed.add_field(name=name, value=value)
-		await ctx.send(embed=embed)
+    @help.command(name="misc")
+    @commands.guild_only()
+    async def misc(self, ctx):
+        embed = self.embed.copy()
+        embed.title = "🧰 Miscellaneous"
+        lines = self.prompts["misc"]["body"]
+        embed.description = '\n'.join(lines)
+        await ctx.reply(embed=embed, mention_author=False)
 
-	@help.command()
-	@commands.guild_only()
-	async def mbc(self, ctx):
-		prefix = Prefixes.fetch_prefix(ctx.guild.id)
-		embed = discord.Embed(title='=MemberCounter Guide=', color=discord.Color.dark_gold())
-		embed.add_field(name='Setup',
-						value=f'`{prefix}mbc (VoiceChannelID)`\nThis sets the tagged voice channel into a membercounter that updates its name into the server member count. Use the COPYID function to copy an id of a voice channel and place it in the <VoiceChannelID> argument.')
-		embed.add_field(name='Check',
-						value=f'`{prefix}mbc check`\nShows you what the currently configured channel for member counter is. If none/disabled, it will return as none.')
-		await ctx.send(embed=embed)
+    @help.command(name="greet")
+    @commands.guild_only()
+    async def greet(self, ctx):
+        embed = self.embed.copy()
+        embed.title = "🙏 Welcome and Goodbye messages"
+        embed.description = self.prompts["greet"]["desc"]
+        await ctx.reply(embed=embed, mention_author=False)
 
-	@help.command()
-	@commands.guild_only()
-	async def level(self, ctx):
-		prefix = Prefixes.fetch_prefix(ctx.guild.id)
-		embed = discord.Embed(title='=Chat Levelling System Guide=', color=discord.Color.dark_gold())
-		embed.add_field(name='Rank',
-						value=f"`{prefix}rank`\nShows you the tagged member's rank in the system, if none is tagged, your rank will be displayed.")
-		await ctx.send(embed=embed)
+    @help.command(name="tag")
+    @commands.guild_only()
+    async def tag(self, ctx):
+        embed = self.embed.copy()
+        embed.title = '🛑 Tag Commands'
+        embed.add_field(name="Global tags",
+                        value="`globtag (name)`", inline=False)
+        embed.add_field(
+            name="Local tags", value="__**[Setup](https://google.com \"Manage server permissions required.\")**__\n`tag add (name) (content)`\n`tag remove (name)`\n\n__**[User](https://google.com \"Default permissions.\")**__\n`tag (name)`", inline=False)
+        await ctx.reply(embed=embed, mention_author=False)
 
-	@help.command()
-	@commands.guild_only()
-	async def tag(self, ctx):
-		prefix = Prefixes.fetch_prefix(ctx.guild.id)
-		embed = discord.Embed(title='=Tag Commands=', color=discord.Color.dark_gold())
-		embed.add_field(name='Global Tags', value=f'`{prefix}gtag <name>`\n This gets the tag with the name from the database.')
-		embed.add_field(name='Tags[DISABLED]',
-						value=f'`{prefix}tag <name>`\n This gets the private tag with the name.\n`{prefix}tag add <name> <content>`\n This creates a new tag.\n`{prefix}tag remove <name>`\n This removes the private tag with the name.')
-		await ctx.send(embed=embed)
+    @help.command(name="typewriter", aliases=['tw'])
+    @commands.guild_only()
+    async def type_writer_help(self, ctx):
+        embed = self.embed.copy()
+        embed.title = "✍️ TypeWriter Commands"
+        embed.description = "Typewriter helps you create embeds to finally package it into a channel. Use the notation `%` to divide the title and the description.\n\n__**[Threading](https://google.com \"Manage channel permissions required.\")**__\n> `advThread`\n> `thread (#channel) (decimal_color_code) (title%description)`\n> `thread preview (decimal_color_code) (title%description)`\n> `thread edit (target object) (#channel) (messageID) (value)`"
+        await ctx.reply(embed=embed, mention_author=False)
 
-	@help.command(aliases=['typewriter', 'typeWriter'])
-	@commands.guild_only()
-	async def TypeWriter(self, ctx):
-		prefix = Prefixes.fetch_prefix(ctx.guild.id)
-		embed = discord.Embed(title='=TypeWriter Commands=', color=discord.Color.dark_gold())
-		embed.add_field(name='Threading', value=f"`{prefix}thread <#channel> <decimal_color_code> <title%description>`\n Creates a customized embed and sends it directly to any specified channel.\n*Use '%' to split title and description*\n *Use '|' to go to new lines.*")
-		embed.add_field(name='AdvanceThreading', value=f"`{prefix}advThread`\n Creates a customized embed step by step and sends it directly to a finally mentioned channel.")
-		
-		embed.add_field(name='Preview',
-						value=f'`{prefix}thread preview <decimal_color_code> <title%description>`\n This works just like the original command but its a preview that sends the embed to the channel the command is used in.')
-		embed.add_field(name='Edit',
-						value=f'`{prefix}thread edit <#channel> <messageID> <decimal_color_code> <title%description>`\n Edits any pre-existing embeds/messages only sent by the bot.')
-		embed.add_field(name='Specified Edit',
-						value=f'`{prefix}thread edit_<target object> <#channel> <messageID> <value>`\n Specifically edits an attribute of any embeds, i.e.color of an embed, description of an embed etc... The input value may vary as the target object changes.')
-		
-		embed.add_field(name='Announce selectively[CUSTOM]', value= f'`{prefix}announce selective <#channel> <message>`\nAnnounces a text message with a pre-built template and mentions everyone with the <@&756865729550090330> role.'),
-				  
-		await ctx.send(embed=embed)
+    @help.command(name="reactionrole", aliases=["rr"])
+    @commands.guild_only()
+    async def rrole(self, ctx):
+        prefix = Prefixes.fetch_prefix(ctx.guild.id)
+        embed = discord.Embed(title='=Reaction Role Guide=', color=self.color)
+        embed.add_field(name='Setup',
+                        value=f"`{prefix}rrole (#channel) (message_id) (emoji) (@role)`\nThis command allows you to setup reactionroles\n(#channel) : _Mention the channel where the message with reactionrole will be_\n(message_id) : _Insert the message ID of the message you'd like people to react to, to get roles_\n(emoji) : _The emoji people would react with to get the role mentioned_\n(@role) : _The role people would get when reacted with the set emoji_")
+        embed.add_field(name='Check',
+                        value=f'`{prefix}rrole check`\n Checks all reaction role objects for your server')
+        embed.add_field(name='Clear',
+                        value=f'`{prefix}welcome clear (message_id)`\n Clear all reaction role objects corresponding to the message ID')
+        await ctx.reply(embed=embed, mention_author=False)
 
-	@help.command()
-	@commands.guild_only()
-	async def rrole(self, ctx):
-		prefix = Prefixes.fetch_prefix(ctx.guild.id)
-		embed = discord.Embed(title='=Reaction Role Guide=', color=discord.Color.dark_gold())
-		embed.add_field(name='Setup',
-						value=f"`{prefix}rrole <#channel> <message_id> <emoji> <@role>`\nThis command allows you to setup reactionroles\n<#channel> : _Mention the channel where the message with reactionrole will be_\n<message_id> : _Insert the message ID of the message you'd like people to react to, to get roles_\n<emoji> : _The emoji people would react with to get the role mentioned_\n<@role> : _The role people would get when reacted with the set emoji_")
-		embed.add_field(name='Check',
-						value=f'`{prefix}rrole check`\n Checks all reaction role objects for your server')
-		embed.add_field(name='Clear',
-						value=f'`{prefix}welcome clear <message_id>`\n Clear all reaction role objects corresponding to the message ID')
-		await ctx.send(embed=embed)
+    @help.command(name="command", aliases=["cmd"])
+    async def command_support(self, ctx, command):
+        command = self.bot.get_command(command)
+        if command is not None:
+            embed = self.embed.copy()
+            embed.title = f"{ctx.prefix}{command}"
+            embed.set_author(name="Pill's command help",
+                             icon_url="https://cdn.discordapp.com/emojis/818820065834303488.png?v=1")
+            embed.description = f"__**Description**__\n```{command.short_doc if command.short_doc not in (None, '') else 'Undocumented'}```\n**`p!code {command}`** to view the source code of the command.\n\n__**Usage**__\n`{ctx.prefix}{command.name}{command.signature}`"
+            if aliases := command.aliases:
+                embed.add_field(name="Aliases", value=", ".join(
+                    aliases), inline=False)
+            await ctx.reply(embed=embed, mention_author=False)
+        if command is None:
+            await ctx.send("command not found")
+
 
 def setup(bot):
-	bot.add_cog(Help(bot))
-	print('Help.cog is loaded')
+    bot.add_cog(Help(bot))
+    print('Help.cog is loaded')
