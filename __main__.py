966344b057dd47f68461ce37056efcb9355bff07
Ricky <66440980+Ricky-MY@users.noreply.github.com>
2021-03-19 18:40:09 +0630

    Extensions are walked through in a different module

--- __main__.py
+++ __main__.py
@@ -1,43 +1,28 @@
-# The Lion Bot
-# Author - Ricky
-# Created at 5th of August 2020
-from discord import Intents
-from discord import Status
-from discord import Game
+from os import getenv
+from dotenv import load_dotenv
+from discord import Intents, Status, Game
 from discord.ext import commands
 
-from dotenv import load_dotenv
-from os import listdir, getenv
-
-from bot.utilities.prefixes import Prefixes
-from bot.moderation.admin import bot_admin_check
+from bot.constants import Defaults
+from bot.utils.extensions import EXTENSIONS
 
 intents = Intents.default()
 intents.reactions = True
-intents.members = True
 
-bot = commands.Bot(command_prefix=Prefixes.get_prefix, intents = intents, status=Status.do_not_disturb, activity=Game("Gerald's game >:)"))
+# Bot constructor
+bot = commands.Bot(command_prefix = Defaults.PREFIX,
+                    intents = intents,
+                    status = Status.do_not_disturb, activity = Game("New Bot"))
+
+# On ready event
 bot.remove_command('help')
-
-# Shut Down Command
-@bot.command(name="shutdown")
-@commands.guild_only()
-@commands.check(bot_admin_check)
-async def shutdown(ctx):
-	await ctx.send('Bot has been shut down.')
-	await bot.logout()
-
 @bot.event
 async def on_ready():
-	print("____________\nPill is on standby\n____________")
+	print(f"{bot.user.name} is on standby")
 
-parentdir = 'bot'
-for filename in listdir(f'./{parentdir}'):
-	if not filename.endswith('.py') and not filename.startswith('_') and not filename.startswith('.'):
-		for subdir in listdir(f'./{parentdir}/{filename}/'):
-			if subdir.endswith('.py') and not subdir.startswith('_') and not subdir.startswith('.'):
-				bot.load_extension(f'{parentdir}.{filename}.{subdir[:-3]}')
+for ext in EXTENSIONS:
+    bot.load_extension(ext)
 
 load_dotenv()
-token = getenv('TOKEN')
-bot.run(token)
+TOKEN = getenv('TOKEN')
+bot.run(TOKEN)
